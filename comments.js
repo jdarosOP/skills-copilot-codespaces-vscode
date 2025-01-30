268c7ee15e9ef766123e1f607ad20b4d95f3c299
jdarosOP <j.da-ros@groupeonepoint.com>
2025-01-30 16:14:18 +0000

    4rd one

--- comments.js
+++ comments.js
@@ -1,61 +1,41 @@
 // create web server
-// npm install express
-// npm install body-parser
-// npm install cookie-parser
-// npm install express-session
-// npm install express-myconnection
-// npm install mysql
-// npm install ejs
-// npm install express-flash
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var flash = require('express-flash');
+var router = express.Router();
+var path = require('path');
+
+// database connection
 var mysql = require('mysql');
-var myConnection = require('express-myconnection');
-var dbOptions = {
+var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
     port: 3306,
-    database: 'comments'
-};
-app.use(myConnection(mysql, dbOptions, 'single'));
-app.use(cookieParser('secret'));
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}));
-app.use(flash());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
-    req.getConnection(function (error, conn) {
-        conn.query('SELECT * FROM comments', function (err, rows, fields) {
-            if (err)
-                throw err;
-            res.render('comments', {
-                title: 'Comments',
-                comments: rows
-            });
-        });
+    user: 'root',
+    password: '1234',
+    database: 'jsman'
+});
+connection.connect();
+
+// set router
+router.post('/form', function(req, res) {
+    console.log(req.body);
+    res.render('email.ejs', {'email': req.body.email});
+});
+
+router.post('/ajax', function(req, res) {
+    var email = req.body.email;
+    var resData = {};
+
+    var query = connection.query('select name from user where email="' + email + '"', function(err, rows) {
+        if(err) throw err;
+        if(rows[0]) {
+            resData.result = "ok";
+            resData.name = rows[0].name;
+        } else {
+            resData.result = "none";
+            resData.name = "";
+        }
+        res.json(resData);
     });
 });
-app.post('/', function (req, res) {
-    var comment = req.body.comment;
-    req.getConnection(function (error, conn) {
-        conn.query('INSERT INTO comments SET ?', { comment: comment }, function (err, result) {
-            if (err)
-                throw err;
-            req.flash('success', 'Comment added successfully.');
-            res.redirect('/');
-        });
-    });
-});
-app.listen(3000, function () {
-    console.log('Server is running at port 3000');
-});
+
+module.exports
